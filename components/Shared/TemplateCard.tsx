722bd72bed59bfde8f9f5ec0c1ba75280e95af67
dogoshi <info@dogoshi.io>
2024-01-15 15:27:21 +0600

    adding trasition on the card

--- components/Shared/TemplateCard.tsx
+++ components/Shared/TemplateCard.tsx
@@ -21,13 +21,13 @@ const TemplateCard: FC<Props> = ({
   return (
     <Link
       href={slug.toString().toLowerCase()}
-      className=" border transition-all duration-500 border-skin-gray-900/5 rounded-lg p-3 relative group space-y-2.5 will-change-transform hover:bg-skin-card-foreground cursor-pointer"
+      className={`border rounded-lg p-3 relative group space-y-2.5 will-change-transform border-skin-gray-900/5 cursor-pointer link-transition`}
     >
       <div className="template-card-image-container">
-        <div className=" absolute top-0 left-0 right-0  bottom-0 rounded-[inherit]">
+        <div className="absolute top-0 left-0 right-0 bottom-0 rounded-[inherit]">
           <Image
             src={feature_image}
-            className=" block w-full h-full object-cover object-center "
+            className="block w-full h-full object-cover object-center"
             style={{
               imageRendering: "auto",
             }}
@@ -39,18 +39,16 @@ const TemplateCard: FC<Props> = ({
       </div>
       <div className="">
         <div className="flex items-center justify-between">
-          <div className=" inline-flex items-center gap-1">
+          <div className="inline-flex items-center gap-1">
             <div>
-              <h5 className="text-sm group-hover:!text-white text-skin-gray-100 font-normal">
+              <h5 className="text-sm group-hover:text-white text-skin-gray-100 font-normal">
                 {title}
               </h5>
             </div>
             <Badge variant={"new"}>new</Badge>
           </div>
           <div>
-            <h5 className=" text-sm text-skin-gray-100 font-normal">
-              ${price}
-            </h5>
+            <h5 className="text-sm text-skin-gray-100 font-normal">${price}</h5>
           </div>
         </div>
         <div>
