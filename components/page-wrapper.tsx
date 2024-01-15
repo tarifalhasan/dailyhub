c3f20d98b107066c3ce6afa03d03c0177e100d90
dogoshi <info@dogoshi.io>
2024-01-15 16:49:07 +0600

    update

--- components/page-wrapper.tsx
+++ components/page-wrapper.tsx
@@ -7,7 +7,6 @@ export default function PageWrapper({ children }: { children: ReactNode }) {
       <AnimatePresence>
         <motion.main
           key={typeof window === "undefined" ? "ssr" : undefined}
-          className="pt-[85px] lg:pt-[105px]"
           initial={{ opacity: 0, y: 15 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 15 }}
